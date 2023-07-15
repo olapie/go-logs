5593542de3995b4413360c3404c9433d84d56c88
yxz <yxzbox@gmail.com>
2023-07-15 19:55:24 -0400

    fast commit

--- logs.go
+++ logs.go
@@ -10,55 +10,49 @@ import (
 
 // Init default logger writing to stderr and filename if it's not empty
 func Init(filename string) io.Closer {
-	var w io.Writer
-	var closer io.Closer
-	if filename != "" {
-		f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND, 0644)
-		if err != nil {
-			log.Fatalln("cannot open file", filename, err)
+	debug := strings.ToLower(os.Getenv("DEBUG"))
+	var options *slog.HandlerOptions
+	if debug == "1" || debug == "true" || debug == "enabled" {
+		options = &slog.HandlerOptions{
+			Level: slog.LevelDebug,
 		}
-		w = io.MultiWriter(f, os.Stderr)
-		closer = f
-	} else {
-		w = os.Stderr
 	}
 
-	var l *slog.Logger
-	debug := strings.ToLower(os.Getenv("DEBUG"))
-	if debug == "1" || debug == "true" || debug == "enabled" {
-		l = slog.New(slog.NewJSONHandler(w, &slog.HandlerOptions{
-			AddSource: true,
-			Level:     slog.LevelDebug,
-		}))
-	} else {
-		l = slog.New(slog.NewJSONHandler(w, &slog.HandlerOptions{
-			AddSource: true,
-			Level:     slog.LevelInfo,
-		}))
+	if filename == "" {
+		if options != nil {
+			slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, options)))
+		}
+		return nil
 	}
-	slog.SetDefault(l)
-	return closer
+
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalln("cannot open file", filename, err)
+	}
+	h := MultiHandler(slog.NewJSONHandler(f, options),
+		slog.NewTextHandler(os.Stderr, options))
+	slog.SetDefault(slog.New(h))
+	return f
 }
 
 // InitRotate init default logger writing to rotated file and stderr
 // If filename is empty, then a default filename is used
 func InitRotate(filename string, optFns ...func(options *RotateFileWriterOptions)) io.Closer {
-	f := NewRotateFileWriter(filename, optFns...)
-	w := io.MultiWriter(f, os.Stderr)
-
-	var l *slog.Logger
-	debug := strings.ToLower(os.Getenv("DEBUG"))
-	if debug == "1" || debug == "true" || debug == "enabled" {
-		l = slog.New(slog.NewJSONHandler(w, &slog.HandlerOptions{
-			AddSource: true,
-			Level:     slog.LevelDebug,
-		}))
-	} else {
-		l = slog.New(slog.NewJSONHandler(w, &slog.HandlerOptions{
-			AddSource: true,
-			Level:     slog.LevelInfo,
-		}))
+	if filename == "" {
+		log.Fatalln("filename is empty")
 	}
-	slog.SetDefault(l)
+
+	debug := strings.ToLower(os.Getenv("DEBUG"))
+	var options *slog.HandlerOptions
+	if debug == "1" || debug == "true" || debug == "enabled" {
+		options = &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}
+	}
+
+	f := NewRotateFileWriter(filename, optFns...)
+	h := MultiHandler(slog.NewJSONHandler(f, options),
+		slog.NewTextHandler(os.Stderr, options))
+	slog.SetDefault(slog.New(h))
 	return f
 }
